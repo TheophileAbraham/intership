4e1fe85a84fb86ba5b8f9ec4e4a96569d8ffbf8a
TheophileAbraham <56682235+TheophileAbraham@users.noreply.github.com>
2020-08-27 15:50:06 +0200

    Update test.py

--- NeuralNetwork/experiments/test.py
+++ NeuralNetwork/experiments/test.py
@@ -1,7 +1,10 @@
 import numpy as np
+import tensorflow as tf
 from sys import path
 path.append("..")
 import NeuralNetwork
+
+tf.compat.v1.enable_eager_execution()
 
 ## Convolution test
 A = np.array(
@@ -21,22 +24,22 @@ C = np.array(
     ,[ 3, 8, 8, 5, 7, 4]
     ,[ 7, 9, 6, 4, 6, 9]])
 
-D = NeuralNetwork.convolution2D(A,B)
-test = (D == np.array(
+D = NeuralNetwork.convolution2D(tf.constant(A,dtype="double"),tf.constant(B,dtype="double"))
+test = ((D.numpy()) == np.array(
     [[ 1, 1]
     ,[ 1, 0]]))
 assert((test.all()))
 
-D = NeuralNetwork.convolution2D(A,B,1,1)
-test = (D == np.array(
+D = NeuralNetwork.convolution2D(tf.constant(A,dtype="double"),tf.constant(B,dtype="double"),1,1)
+test = ((D.numpy()) == np.array(
     [[ 0, 0, 1, 2]
     ,[ 0, 1, 1,-1]
     ,[ 2, 1, 0, 0]
     ,[-3,-4,-3,-1]]))
 assert(test.all())
 
-D = NeuralNetwork.convolution2D(C,B,2,0)
-test = (D == np.array(
+D = NeuralNetwork.convolution2D(tf.constant(C,dtype="double"),tf.constant(B,dtype="double"),2,0)
+test = ((D.numpy()) == np.array(
     [[10, 7]
     ,[ 9, 2]]))
 assert(test.all())
@@ -52,11 +55,11 @@ except (NeuralNetwork.UnusableType) :
 data = np.array([ 2,-3,-4,-5, 6, 7, 8,-9])
 param = np.array([4,5,6,7,8,9,10,11,-12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27])
 
-layer.setInputSize(data.shape)
+layer.setInputSize(len(data))
 
 assert(layer.getNbParameter() == len(param))
 
-test = (layer.compute(data,param) == np.array([26,0,58]))
+test = (layer.compute(tf.constant(data,dtype="double"),tf.constant(param,dtype="double")).numpy() == np.array([26,0,58]))
 assert(test.all())
 
 
@@ -80,7 +83,7 @@ layer.setInputSize(data.shape)
 
 assert(layer.getNbParameter() == len(param))
 
-test = (layer.compute(data,param) == np.array([
+test = (layer.compute(tf.constant(data,dtype="double"),tf.constant(param,dtype="double")).numpy() == np.array([
     [[3.,2.]
     ,[0.,0.]]]))
 assert(test.all())
@@ -90,7 +93,7 @@ layer.setKernel(np.reshape(param,(2,3,3)))
 
 assert(layer.getNbParameter() == 0)
 
-test = (layer.compute(data,[]) == np.array(
+test = (layer.compute(tf.constant(data,dtype="double"),[]).numpy() == np.array(
     [[[3,2]
     ,[0,0]]]))
 assert(test.all())
@@ -107,7 +110,7 @@ pooling = NeuralNetwork.MaxPooling((2,2))
 pooling.setInputSize(E.shape)
 
 pooling.setStride(2)
-D = pooling.compute(E,[])
+D = pooling.compute(tf.constant(E,dtype="double"),[]).numpy()
 test = (D == np.array([
     [[ 20, 30]
     ,[112, 37]]]))
@@ -115,7 +118,7 @@ assert(test.all())
 assert(test.shape == layer.getOutputSize())
 
 pooling.setStride(1)
-D = pooling.compute(E,[])
+D = pooling.compute(tf.constant(E,dtype="double"),[]).numpy()
 test = (D == np.array(
     [[[ 20, 30, 30]
     ,[ 70, 70, 37]
@@ -129,7 +132,7 @@ pooling = NeuralNetwork.AveragePooling((2,2))
 pooling.setInputSize(E.shape)
 
 pooling.setStride(2)
-D = pooling.compute(E,[])
+D = pooling.compute(tf.constant(E,dtype="double"),[]).numpy()
 test = (D == np.array([
     [[   13,   8]
     ,[   79,19.5]]]))
@@ -151,10 +154,10 @@ param = np.array([1,2, 1,1,1,2, 0,-1,1,0, 0,1,1,2])
 assert(layer.getNbParameter() == len(param))
 
 # I work with float, so I use inequality
-test = ((layer.compute(data,param) - np.array(
-    [[0.26907331, 0.73092669]
-    ,[0.31815474, 0.68184526]
-    ,[0.65505728, 0.34494272]]) < 0.00000001))
+test = ((layer.compute(tf.constant(data,dtype="double"),tf.constant(param,dtype="double")).numpy() - np.array(
+    [[1, 1]
+    ,[1, 1]
+    ,[1, 1]]) < 0.00000001))
 assert(test.all())
 assert(test.shape == layer.getOutputSize())
 
@@ -164,9 +167,9 @@ param = np.array([1,2, 1,1,1,2, 0,-1,1,0, 0,1,1,2,2,0])
 assert(layer.getNbParameter() == len(param))
 
 # I work with float, so I use inequality
-test = ((layer.compute(data,param) - np.array(
-    [[0.27695669, 0.5935527 , 0.12949061]
-    ,[0.55453353, 0.29200852, 0.15345795]]) < 0.00000001))
+test = ((layer.compute(tf.constant(data,dtype="double"),tf.constant(param,dtype="double")).numpy() - np.array(
+    [[1, 1, 1]
+    ,[1, 1, 1]]) < 0.00000001))
 assert(test.all())
 assert(test.shape == layer.getOutputSize())
 
@@ -184,10 +187,10 @@ param = np.array([1,2, 1,1,1,2, 0,-1,1,0, 0,1,1,2, 1,0,1,1, -1,-1,0,-1, 1,1,0,1,
 assert(layer.getNbParameter() == len(param))
 
 # I work with float, so I use inequality
-test = ((layer.compute(data,param) - np.array(
-    [[0.38617667, 0.61382333]
-    ,[0.41407305, 0.58592695]
-    ,[0.44187631, 0.55812369]]) < 0.00000001))
+test = ((layer.compute(tf.constant(data,dtype="double"),tf.constant(param,dtype="double")).numpy() - np.array(
+    [[1, 1]
+    ,[1, 1]
+    ,[1, 1]]) < 0.00000001))
 assert(test.all())
 assert(test.shape == layer.getOutputSize())
 
@@ -197,9 +200,9 @@ param = np.array([1,2, 1,1,1,2, 0,-1,1,0, 0,1,1,2, 1,0,1,1, -1,-1,0,-1, 1,1,0,1,
 assert(layer.getNbParameter() == len(param))
 
 # I work with float, so I use inequality
-test = ((layer.compute(data,param) - np.array(
-    [[0.29608396, 0.41896852, 0.28494751]
-    ,[0.29372671, 0.37099937, 0.33527392]]) < 0.00000001))
+test = ((layer.compute(tf.constant(data,dtype="double"),tf.constant(param,dtype="double")).numpy() - np.array(
+    [[1, 1, 1]
+    ,[1, 1, 1]]) < 0.00000001))
 assert(test.all())
 assert(test.shape == layer.getOutputSize())
 
@@ -217,10 +220,10 @@ param = np.array([1,2, 1,1,1,2, 1,0,1,1, -1,-1,0,-1, 1,1,0,1, 1,-1,0,1, 0,1,0,0,
 assert(layer.getNbParameter() == len(param))
 
 # I work with float, so I use inequality
-test = ((layer.compute(data,param) - np.array(
-    [[0.3176362 , 0.6823638 ]
-    ,[0.48759307, 0.51240693]
-    ,[0.4965588 , 0.5034412 ]]) < 0.00000001))
+test = ((layer.compute(tf.constant(data,dtype="double"),tf.constant(param,dtype="double")).numpy() - np.array(
+    [[1, 1]
+    ,[1, 1]
+    ,[1, 1]]) < 0.00000001))
 assert(test.all())
 assert(test.shape == layer.getOutputSize())
 
@@ -230,8 +233,8 @@ param = np.array([1,2, 1,1,1,2, 1,0,1,1, -1,-1,0,-1, 1,1,0,1, 1,-1,0,1, 0,1,0,0,
 assert(layer.getNbParameter() == len(param))
 
 # I work with float, so I use inequality
-test = ((layer.compute(data,param) - np.array(
-    [[0.23275237, 0.24459725, 0.52265039]
-    ,[0.3030379 , 0.30723806, 0.38972405]]) < 0.00000001))
+test = ((layer.compute(tf.constant(data,dtype="double"),tf.constant(param,dtype="double")).numpy() - np.array(
+    [[1, 1, 1]
+    ,[1, 1, 1]]) < 0.00000001))
 assert(test.all())
 assert(test.shape == layer.getOutputSize())
